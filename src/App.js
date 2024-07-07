3db966e980c690b0320e8aa939edd317613f7b51
nurmuhammad <juraqobilovnurmuhammad9888@gmail.com>
2024-07-07 10:12:24 +0500

    add  login page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Typography</h1>
+      <div className='container'>
+        <h1>Typography</h1>
+      </div>
     </div>
   );
 }
