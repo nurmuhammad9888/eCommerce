ae5bf33ee1abc8ede364580d2ac5750d4893cd2f
nurmuhammad <juraqobilovnurmuhammad9888@gmail.com>
2024-07-24 02:50:49 +0500

    add new elemets

--- src/pages/Account/OrderHistory.js
+++ src/pages/Account/OrderHistory.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import "./orderhistory.css";
+import { Link } from 'react-router-dom';
 
 export const OrderHistory = () => {
-
+nb 
     const order =[
         {
             id: 738,
@@ -26,7 +27,26 @@ export const OrderHistory = () => {
 
     return (
         <div className='order-history'>
-            
+            <div className="order-history-title-wrap">
+                <h3 className="order-history-title">Recet Order History</h3>
+                <Link className='order-history-link' to={"/"} >View All</Link>
+            </div>
+            <div className="order-table-hedaer-wrap">
+                <p className="order-table-hedaer">Order ID</p>
+                <p className="order-table-hedaer">Date</p>
+                <p className="order-table-hedaer">Total</p>
+                <p className="order-table-hedaer">Status</p>
+                <p className="order-table-hedaer"></p>
+            </div>
+            <ul className="order-history-list">
+                <li className="order-history-item">
+                    <span className='order-history-item-id'></span>
+                    <span className='order-history-item-data'></span>
+                    <span className='order-history-item-total'></span>
+                    <span className='order-history-item-status'></span>
+                    <Link to={'/'} className='order-history-item-id'>View Details</Link>
+                </li>
+            </ul>
         </div>
     )
 }
